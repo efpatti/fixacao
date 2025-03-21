adedd2fd30203ba3ec3b6d192dc0fcba033587ac
Enzo Patti <enzo.patti@aluno.senai.br>
2025-03-20 22:01:40 -0300

    trapezio

--- area_trapezio.js
+++ area_trapezio.js
@@ -5,18 +5,37 @@ const rl = readline.createInterface({
  output: process.stdout,
 });
 
-rl.question("Valor do lado da base menor: ", (baseMenor) => {
- if (isNaN(baseMenor) || baseMenor === "") {
+rl.question("Valor da base maior: ", (baseMaior) => {
+ if (isNaN(baseMaior) || baseMaior === "") {
   console.log("Por favor, insira um número válido.");
- } else {
-    rl.question("Valor do lado da base menor: ", (baseMenor) => {
-        if (isNaN(baseMenor) || baseMenor === "") {
-         console.log("Por favor, insira um número válido.");
-        } }
-  const ladoNumerico = parseFloat(lado);
-  let areaTrapezio = (baseMaior + baseMenor) * altura;
-  console.log("A área do quadrado é: " + areaTrapezio);
+  rl.close();
+  return;
  }
 
- rl.close();
+ rl.question("Valor da base menor: ", (baseMenor) => {
+  if (isNaN(baseMenor) || baseMenor === "") {
+   console.log("Por favor, insira um número válido.");
+   rl.close();
+   return;
+  }
+
+  rl.question("Valor da altura: ", (altura) => {
+   if (isNaN(altura) || altura === "") {
+    console.log("Por favor, insira um número válido.");
+    rl.close();
+    return;
+   }
+
+   // Convertendo as entradas para números flutuantes
+   const baseMaiorNum = parseFloat(baseMaior);
+   const baseMenorNum = parseFloat(baseMenor);
+   const alturaNum = parseFloat(altura);
+
+   // Calculando a área do trapézio
+   let areaTrapezio = ((baseMaiorNum + baseMenorNum) * alturaNum) / 2;
+   console.log("A área do trapézio é: " + areaTrapezio);
+
+   rl.close();
+  });
+ });
 });
