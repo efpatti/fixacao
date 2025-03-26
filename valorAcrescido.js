4c3fa699575e9f31ea121f65dfd36c6d4448b852
Enzo Patti <enzo.patti@aluno.senai.br>
2025-03-26 11:57:56 -0300

    interpolacao de strings valorAcrescimo

--- valorAcrescido.js
+++ valorAcrescido.js
@@ -6,39 +6,37 @@ const rl = readline.createInterface({
 });
 
 rl.question("Digite o valor de um produto: ", (valorProduto) => {
- if (isNaN(valorProduto) || valorProduto === "" || valorProduto < 0) {
+ const valorProdutoReal = parseFloat(valorProduto);
+
+ if (isNaN(valorProdutoReal) || valorProdutoReal < 0) {
   console.log("Digite um valor válido!");
-  rl.close;
+  rl.close();
   return;
  }
+
  rl.question("Digite a porcentagem de acréscimo: ", (acrescimoDigitado) => {
-  if (
-   isNaN(acrescimoDigitado) ||
-   acrescimoDigitado === "" ||
-   acrescimoDigitado < 0
-  ) {
+  const porcentagem = parseFloat(acrescimoDigitado);
+
+  if (isNaN(porcentagem) || porcentagem < 0) {
    console.log("Digite um acréscimo válido!");
    rl.close();
    return;
   }
-  const valorProdutoReal = parseFloat(valorProduto);
-  const porcentagem = parseFloat(acrescimoDigitado);
-  const porcentagemReal = porcentagem / 100;
-  const acrescimo = valorProdutoReal * porcentagemReal;
+
+  const acrescimo = (valorProdutoReal * porcentagem) / 100;
   const valorProdutoAcrescido = valorProdutoReal + acrescimo;
+
   console.log(
-   valorProdutoReal,
-   " acrescentando ",
-   porcentagem,
-   "% da ",
-   acrescimo,
-   " de acréscimo."
+   `O valor do produto é R$ ${valorProdutoReal.toFixed(
+    2
+   )}, com acréscimo de ${porcentagem}%.`
   );
   console.log(
-   "Logo, " + valorProdutoReal + " + ",
-   acrescimo,
-   " = ",
-   valorProdutoAcrescido
+   `Acréscimo: R$ ${acrescimo.toFixed(
+    2
+   )}\nTotal: R$ ${valorProdutoAcrescido.toFixed(2)}`
   );
+
+  rl.close();
  });
 });
